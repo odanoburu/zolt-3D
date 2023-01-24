4d67b2bd8e0accd311f11b7fcd53cbdc904c737f
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2023-01-24 16:48:25 -0300

    More

--- zp.lean
+++ zp.lean
@@ -1,25 +1,45 @@
 #check Prod
 
-namespace Zp
+namespace ZpInd
 
 structure P : Type
 
-structure S : Type where
-  p1 : P
-  p2 : P
-  neq : p1 ≠ p2
-
--- inductive S : Type where
--- | s₁ : (n : P) → (m : P) → n ≠ m → S
--- | s₂ : (p : S)
---      → (q : S)
---      -- ¬ Colinear p q → (p ∩ q : P)
---      → ∃ x : P, (x ∈ p ∧ x ∈ q ∧ ¬ (∃ y : P, y ≠ x ∧ y ∈ p ∧ y ∈ q))
---      -- → ¬ (∃ x, ∃ y, x ∈ p ∧ x ∈ q ∧ y ∈ p ∧ y ∈ q ∧ x ≠ y)
---      → S
+inductive S : Type where
+| s₁ : (n : P) → (m : P) → n ≠ m → S
+| s₂ : (p : S)
+     → (q : S)
+     -- -- ¬ Colinear p q → (p ∩ q : P)
+     -- → ∃ x : P, (x ∈ p ∧ x ∈ q ∧ ¬ (∃ y : P, y ≠ x ∧ y ∈ p ∧ y ∈ q))
+     -- -- → ¬ (∃ x, ∃ y, x ∈ p ∧ x ∈ q ∧ y ∈ p ∧ y ∈ q ∧ x ≠ y)
+     → S
 
 inductive F : Type where
 | f₁ : (p : S)
      → (q : S)
      -- → Jordan(p∪q)
      → F
+| f₂ : (p : F)
+     → (q : F)
+     -- → p ∩ q : F
+     → F
+
+inductive V : Type where
+| v₁ : (p : F)
+     → (q : F)
+     -- Closed(p∪q)
+     → V
+| v₂ : (p : V)
+     → (q : V)
+     -- → (p ∩ q)
+     → V
+
+-- inductive le : Nat → Nat → Prop
+-- | refl : ∀ {m}, le m m
+-- | step : ∀ {m n}, le m n → le m (succ n)
+-- inductive Nat.le (n : Nat) : Nat → Prop
+--   /-- Less-equal is reflexive: `n ≤ n` -/
+--   | refl     : Nat.le n n
+--   /-- If `n ≤ m`, then `n ≤ m + 1`. -/
+--   | step {m} : Nat.le n m → Nat.le n (succ m)
+
+end ZpInd
