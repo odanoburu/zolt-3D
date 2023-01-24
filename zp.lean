0df835811f484c494996ca1a47d7a6dc8e6e3e77
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2023-01-24 19:56:35 -0300

    Some advancement?

--- zp.lean
+++ zp.lean
@@ -1,17 +1,37 @@
-#check Prod
+#check set
+
+section pairwise
+variable (R : α → α → Prop)
+
+/-- `pairwise R l` means that all the elements with earlier indexes are
+  `R`-related to all the elements with later indexes.
+     pairwise R [1, 2, 3] ↔ R 1 2 ∧ R 1 3 ∧ R 2 3
+  For example if `R = (≠)` then it asserts `l` has no duplicates,
+  and if `R = (<)` then it asserts that `l` is (strictly) sorted. -/
+inductive pairwise : List α → Prop
+| nil : pairwise []
+| cons : ∀ {a : α} {l : List α}, (a' ∈ l → R a a') → pairwise l → pairwise (a :: l)
+
+end pairwise
 
 namespace ZpInd
 
-structure P : Type
+structure Point : Type
 
-inductive S : Type where
-| s₁ : (n : P) → (m : P) → n ≠ m → S
-| s₂ : (p : S)
-     → (q : S)
-     -- -- ¬ Colinear p q → (p ∩ q : P)
-     -- → ∃ x : P, (x ∈ p ∧ x ∈ q ∧ ¬ (∃ y : P, y ≠ x ∧ y ∈ p ∧ y ∈ q))
-     -- -- → ¬ (∃ x, ∃ y, x ∈ p ∧ x ∈ q ∧ y ∈ p ∧ y ∈ q ∧ x ≠ y)
-     → S
+structure Segment : Type where
+  p1 : Point
+  p2 : Point
+  neq : p1 ≠ p2
+
+def segmentHasPointIntersection : Segment → Segment → Prop := sorry
+
+-- something strange in the definition is that if weird s is a
+-- polyline, then what's the definition of colinear for polylines?
+-- and if it's just a regular line, then s₂ doesn't make much sense…
+structure PolySegment : Type where
+  init : Segment × Segment
+  more : List Segment
+  joint : ∀ {s : Segment}, s ∈ (init.fst :: init.snd :: segments) → r ∈ (init.fst :: init.snd :: segments) → segmentHasPointIntersection s r
 
 inductive F : Type where
 | f₁ : (p : S)
@@ -43,3 +63,9 @@ inductive V : Type where
 --   | step {m} : Nat.le n m → Nat.le n (succ m)
 
 end ZpInd
+
+namespace ZpAx
+
+
+
+end ZpAx
